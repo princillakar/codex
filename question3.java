263510e250f5aa98d6cb872043014093fd096740
princillakar <53991392+princillakar@users.noreply.github.com>
2019-08-28 19:10:20 +0530

    Update question3.java

--- question3.java
+++ question3.java
@@ -1,30 +1,14 @@
 
 public class question3 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-long num = 600851475143L;
-
-long factor=1;
-while (num>1) {
-	factor++;
-	if (primeCheck(factor)== true) {
-		while (num%factor==0) {
-			num/=factor;
-			
-	}
-}
-	}		
-System.out.println(factor);
-	
-	}
-	public static boolean primeCheck (long x) {
-		for (int div =2; div  <=Math.sqrt(x); div++ ) {
-			if(x% div ==0) {
-				return false;
-				
-			}
-		}
-		return true;
-	}
-
+	public static void main(String[] args){
+		long n= 600851475143L;
+        for(long i=2L; i<n; i++)
+        {
+            while(n%i == 0)
+            {
+                n = n/i;
+            }
+        }
+        System.out.println(n);
+    }
 }

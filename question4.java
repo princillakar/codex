3683a65743894b293fb536f822ee576b301359bd
princillakar <53991392+princillakar@users.noreply.github.com>
2019-08-29 21:04:57 +0530

    Update question4.java

--- question4.java
+++ question4.java
@@ -1,38 +1,27 @@
-public class question4{
+public class question4
 
-public boolean isPalindrome( String s )
     {
-        //if odd length then remove middle character
-        if ( s.length() % 2 != 0 )
-        {
-            s = s.substring( 0, s.length() / 2 ) + s.substring( (s.length() / 2) + 1);
-        }
-
-        return new StringBuffer( s.substring( 0, s.length() / 2) ).
-                                 reverse().toString().equals( s.substring( s.length() / 2) );
-    }
-
-
-public static void main(String[] args)
-{
-    question4 e = new question4();
-    long biggest = 0;
-
-    int a = 999, b = 999;
-    for (int bb = b; bb > 0; bb--)
-    {
-        for (int aa = a; aa > 0; aa--)
-        {
-            if ( e.isPalindrome( new Long(aa*bb).toString() ) )
-            {
-                if ( aa*bb > biggest )
-                {
-                    biggest = aa*bb;
-                }
-            }
-        }
-    }
-
-    System.out.println(biggest);
-}
+       public static void main(String args[])
+	{
+		int pal=0, n, maxPal=0, product;
+		for(int i=999;i>=0;i--)
+		{
+			for(int j=999;j>=0;j--)
+			{
+				pal=0;
+				product=i*j;
+				n=product;
+				while(n>0)
+				{
+					pal=pal*10+n%10;
+					n=n/10;
+				}	
+				if(product==pal && pal>maxPal)
+				{
+					maxPal=pal;
+				}
+			}
+		}
+		System.out.println("Largest Palindrome Number: "+maxPal);
+	}
 }

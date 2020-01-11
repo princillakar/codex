d72641ed4ce0ac4dc0ef47665ac1519fb18463a3
princillakar <53991392+princillakar@users.noreply.github.com>
2020-01-11 08:23:53 +0530

    Update question6.java

--- question6.java
+++ question6.java
@@ -2,14 +2,11 @@ public class question6
 {
     public static void main(String args[]) {
 
-	 int N = 100;
-	
-		int sum = 0;
-		int sum2 = 0;
-		for (int i = 1; i <= N; i++) {
-			sum += i;
-			sum2 += i * i;
-		}
-		System.out.println(sum * sum - sum2);
-	} 
+	 int sum = 0;
+         int square = 0;
+         int n = 100;
+		 sum = n * (n+1)/ 2;
+		 square = (n * (n + 1) * (2 * n + 1)) / 6;
+		 System.out.println(sum * sum - square);	 
+	   }
 }

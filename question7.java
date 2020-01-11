8a362f043203f34fa2a3a198111aa8eded984b19
princillakar <53991392+princillakar@users.noreply.github.com>
2020-01-11 21:02:47 +0530

    Update question7.java

--- question7.java
+++ question7.java
@@ -2,30 +2,36 @@ public class question7 {
 
   
 
-    public static void main(String[] args) {
-        System.out.println(nthPrime(10001));
-    }
-    
-  
-    public static long nthPrime(long n) {
-        int numberOfPrimes = 0;
-        long prime = 1;
-        
-        while (numberOfPrimes < n) {
-            prime++;
-            if (isPrime(prime)) {
-                numberOfPrimes++;
-            }
-        }
-        return prime;
-    }
-    
-      public static boolean isPrime(long n) {
-        if (n < 2) return false;
-        else if (n == 2) return true;
-        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
-            if (n % i == 0)
-                return false;
-        return true;
-    }
-}
+   	 public static void main(String args[])
+	    {
+	        int count=1;
+	        long i=1, lastPrime=-1;
+	        while(count<=10001){
+	            if(isPrime(i))
+	            {
+	                lastPrime=i;
+	                count++;
+	            }
+	            i++;
+	        }
+	        System.out.println(lastPrime);
+	    }
+
+		 static boolean isPrime(long i)
+		    {
+		        if(i<=1)
+		            return false;
+		        if(i<=3)
+		            return true;
+		        if(i%2==0 || i%3==0)
+		            return  false;
+		
+		     for(int j=5; j*j<=i; j+=6)
+		        {
+		            if(i%j==0 || i%(j+2)==0)
+		                return false;
+		        }
+
+		        return true;
+		    }
+	    }

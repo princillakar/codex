04f4835e08cea423c951745a07c07667f531501e
princillakar <53991392+princillakar@users.noreply.github.com>
2020-01-12 09:34:58 +0530

    Update question7.java

--- question7.java
+++ question7.java
@@ -2,7 +2,7 @@ public class question7 {
 
   
 
-   	 public static void main(String args[])
+   	public static void main(String args[])
 	    {
 	        int count=1;
 	        long i=1, lastPrime=-1;
@@ -12,15 +12,13 @@ public class question7 {
 	                lastPrime=i;
 	                count++;
 	            }
-	            i++;
+	            i+=2;
 	        }
 	        System.out.println(lastPrime);
 	    }
 
 		 static boolean isPrime(long i)
 		    {
-		        if(i<=1)
-		            return false;
 		        if(i<=3)
 		            return true;
 		        if(i%2==0 || i%3==0)
@@ -28,10 +26,11 @@ public class question7 {
 		
 		     for(int j=5; j*j<=i; j+=6)
 		        {
-		            if(i%j==0 || i%(j+2)==0)
+		            if(i%j==0 || i%(j+2)==0) 
+		            	
 		                return false;
 		        }
 
-		        return true;
-		    }
+		        return true;		    
+		 }
 	    }
